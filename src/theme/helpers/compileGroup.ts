5ec0e8e4eb08f1281672f3830bfd3a4a10296e20
Thomas Grey <tgreyuk@gmail.com>
2017-06-21 16:18:51 +0100

    Improve layout of Object Literal members

--- src/theme/helpers/compileGroup.ts
+++ src/theme/helpers/compileGroup.ts
@@ -1,11 +1,17 @@
 import { ReflectionGroup } from 'typedoc/dist/lib/models/ReflectionGroup';
+import { ReflectionKind } from 'typedoc/dist/lib/models/reflections/index';
 import { compileTemplate } from '../utils';
 
 export function compileGroup(group: ReflectionGroup) {
+
   let md: hbs.SafeString = '';
 
   if (!group.allChildrenArePrivate) {
-    md = compileTemplate(`partials/members.group.hbs`, group);
+    let displayTitle = true;
+    if (group.kind === ReflectionKind.ObjectLiteral) {
+      displayTitle = false;
+    }
+    md = compileTemplate(`partials/members.group.hbs`, Object.assign(group, {displayTitle}));
   }
 
   return md;
