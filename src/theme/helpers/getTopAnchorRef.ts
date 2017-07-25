442bd884c1ae29452f3daa2de6c761562f0aa20a
Thomas Grey <tgreyuk@gmail.com>
2017-07-25 09:28:00 +0100

    Removed files

--- src/theme/helpers/getTopAnchorRef.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Returns the anchor ref of the top element
- */
-import { ThemeService } from '../service';
-
-export function getTopAnchorRef() {
-  return ThemeService.getAnchorRef(ThemeService.projectName);
-}
