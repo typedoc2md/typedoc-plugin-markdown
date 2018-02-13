0d2ebaea4996a88106cc1f9da6dc23b00d904e58
Thomas Grey <tgreyuk@gmail.com>
2018-02-13 23:02:59 +0000

    Fixed issue with private properties not rendering

--- src/theme/helpers/ifGroupContainesVisibleItems.ts
+++ src/theme/helpers/ifGroupContainesVisibleItems.ts
@@ -1,11 +1,14 @@
 import { ReflectionGroup } from 'typedoc/dist/lib/models/ReflectionGroup';
+import { ThemeService } from '../service';
+
 /**
  * Returns true if group contains visible items
  * @param group
  * @param opts
  */
 export function ifGroupContainesVisibleItems(group: ReflectionGroup, opts: any) {
-  if (!group.allChildrenArePrivate) {
+  const options = ThemeService.getOptions();
+  if (!options.excludePrivate || !group.allChildrenArePrivate) {
     return opts.fn(this);
   } else {
     return opts.inverse(this);
