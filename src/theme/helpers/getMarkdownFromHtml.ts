9399234c8edaaf24e802b9f12200a3f48dc03382
Thomas Grey <tgreyuk@gmail.com>
2018-03-30 21:47:18 +0100

    Migration to turndown

--- src/theme/helpers/getMarkdownFromHtml.ts
+++ src/theme/helpers/getMarkdownFromHtml.ts
@@ -1,9 +1,10 @@
-import * as toMarkdown from 'to-markdown';
+const TurndownService = require('turndown');
+const turndownService = new TurndownService();
 
 /**
  * Coverts html to markdown. We need this in comment blocks that have been processed by the 'Marked' plugin.
  * @param options
  */
 export function getMarkdownFromHtml(options: any) {
-  return toMarkdown(options.fn(this));
+  return turndownService.turndown(options.fn(this));
 }
