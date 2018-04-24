8ec4214290f39dab19d9c093a23a70766483f4ea
Thomas Grey <tgreyuk@gmail.com>
2018-04-24 12:22:18 +0100

    Fixed issue with members not displaying in index

--- src/theme/helpers/ifDisplayIndexItem.ts
+++ src/theme/helpers/ifDisplayIndexItem.ts
@@ -1,5 +1,4 @@
 import { DeclarationReflection } from 'typedoc/dist/lib/models/reflections';
-import { ReflectionKind } from 'typedoc/dist/lib/models/reflections/index';
 import { ThemeService } from '../theme.service';
 
 /**
@@ -9,8 +8,7 @@ import { ThemeService } from '../theme.service';
  */
 export function ifDisplayIndexItem(item: DeclarationReflection, opts: any) {
   const options = ThemeService.getOptions();
-  if ((!item.anchor && !item.groups && item.kind !== ReflectionKind.Interface) ||
-    (options.excludePrivate && item.flags.isPrivate)) {
+  if (options.excludePrivate && item.flags.isPrivate) {
     return opts.inverse(this);
   } else {
     return opts.fn(this);
