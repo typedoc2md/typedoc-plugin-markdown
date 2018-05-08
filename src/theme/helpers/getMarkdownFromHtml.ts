8c0a6192b2c07cbad7ba366532d57e26b874b218
Remy Rylan <remy@remyrylan.com>
2018-05-08 13:41:11 -0400

    Correct turndown configuration

--- src/theme/helpers/getMarkdownFromHtml.ts
+++ src/theme/helpers/getMarkdownFromHtml.ts
@@ -1,12 +1,12 @@
 const TurndownService = require('turndown');
-const turndownService = new TurndownService();
+const turndownService = new TurndownService({
+  codeBlockStyle: 'fenced',
+});
 
 /**
  * Coverts html to markdown. We need this in comment blocks that have been processed by the 'Marked' plugin.
  * @param options
  */
 export function getMarkdownFromHtml(options: any) {
-  return turndownService.turndown(options.fn(this), {
-    codeBlockStyle: 'fenced',
-  });
+  return turndownService.turndown(options.fn(this));
 }
