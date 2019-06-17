7e80aa040e6d71a3ac08c2626f107b2db4716342
tgreyuk <tgreyuk@gmail.com>
2019-06-18 00:39:07 +0100

    Added flags and default valiue to parameter declaration

--- src/lib/theme/helpers/parameter-name-and-type.ts
+++ src/lib/theme/helpers/parameter-name-and-type.ts
@@ -1,12 +1,19 @@
 import { ParameterReflection } from 'typedoc';
+
 import { type } from './type';
 
 export function parameterNameAndType(this: ParameterReflection) {
   const md = [];
   md.push('■');
+  if (this.flags && !this.flags.isRest) {
+    md.push(this.flags.map(flag => `\`${flag}\` `));
+  }
   md.push(`\`${this.flags.isRest ? '...' : ''} ${this.name}\``);
   if (this.type) {
     md.push(`: *${type.call(this.type)}*`);
+  }
+  if (this.defaultValue) {
+    md.push(`= ${this.defaultValue}`);
   }
   return md.join('');
 }
