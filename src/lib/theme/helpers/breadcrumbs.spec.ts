887075dadaec189b6d506064049b1e3b73481d49
tgreyuk <tgreyuk@gmail.com>
2019-06-28 21:35:54 +0100

    Updated specs

--- src/lib/theme/helpers/breadcrumbs.spec.ts
+++ src/lib/theme/helpers/breadcrumbs.spec.ts
@@ -1,12 +1,22 @@
+import { mocked } from 'ts-jest/utils';
+
+import { MarkdownPlugin } from '../../plugin';
+import { MarkdownTheme } from '../theme';
 import { breadcrumbs } from './breadcrumbs';
 
 jest.mock('../../plugin');
 
 describe(`breadcrumbs helper`, () => {
   test(`should compie`, () => {
+    mocked(MarkdownPlugin).settings = {};
+    mocked(MarkdownPlugin).theme = { hasGlobalsFile: true } as MarkdownTheme;
     const page = {
       project: { name: 'typedoc-plugin-markdown', url: 'globals.md' },
-      model: { name: 'Animal', parent: { name: 'Classes', url: 'modules/_classes_.md', parent: {} }, url: 'classes/_classes_.animal.md' },
+      model: {
+        name: 'Animal',
+        parent: { name: 'Classes', url: 'modules/_classes_.md', parent: {} },
+        url: 'classes/_classes_.animal.md',
+      },
     };
     expect(breadcrumbs.call(page)).toMatchSnapshot();
   });
