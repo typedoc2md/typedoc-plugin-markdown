c446dd4dc445629986033088203a46edc815b580
tgreyuk <tgreyuk@gmail.com>
2019-09-12 09:25:26 +0100

    Removed class dependancies

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,53 +1,41 @@
 import * as path from 'path';
+import { Renderer } from 'typedoc';
 import { Converter } from 'typedoc/dist/lib/converter';
 import { Component, ConverterComponent } from 'typedoc/dist/lib/converter/components';
-import { Renderer } from 'typedoc/dist/lib/output/renderer';
-
-import { BitbucketTheme } from './themes/bitbucket.theme';
-import { DocusaurusTheme } from './themes/docusaurus.theme';
-import { GitbookTheme } from './themes/gitbook.theme';
-import { MarkdownTheme } from './themes/markdown.theme';
-import { VuePressTheme } from './themes/vuepress.theme';
-
-type ThemeInstance = MarkdownTheme | DocusaurusTheme | VuePressTheme | BitbucketTheme | GitbookTheme;
 
 @Component({ name: 'markdown' })
 export class MarkdownPlugin extends ConverterComponent {
   initialize() {
     this.listenTo(this.owner, {
-      [Converter.EVENT_RESOLVE_BEGIN]: this.onBegin,
+      [Converter.EVENT_BEGIN]: this.onBegin,
+      [Converter.EVENT_RESOLVE_BEGIN]: this.onResolveBegin,
     });
   }
 
+  /**
+   * Overide the default assets for any custom themes to inherit
+   */
   onBegin() {
-    const renderer = this.application.renderer;
-    const options = this.application.options;
-    const themeName = options.getValue('theme');
-    const themePath = path.join(__dirname, './theme');
-
-    const platform = options.getValue('platform');
-    const theme = themeName === 'markdown' ? this.getTheme(platform, renderer, themePath, options) : null;
-
-    if (theme) {
-      renderer.theme = renderer.addComponent('theme', theme);
-    }
+    Renderer.getDefaultTheme = () => path.join(__dirname, 'theme', 'resources');
   }
 
-  getTheme(platform: string, renderer: Renderer, themePath: string, options: any): ThemeInstance {
-    if (platform) {
-      if (platform === 'gitbook') {
-        return new GitbookTheme(renderer, themePath, options);
-      }
-      if (platform === 'docusaurus') {
-        return new DocusaurusTheme(renderer, themePath, options);
-      }
-      if (platform === 'bitbucket') {
-        return new BitbucketTheme(renderer, themePath, options);
-      }
-      if (platform === 'vuepress') {
-        return new VuePressTheme(renderer, themePath, options);
-      }
+  /**
+   * Read the theme option and load the paths of any recognised built in themes
+   * Otherwise pass the path through to the Renderer
+   */
+  onResolveBegin() {
+    const options = this.application.options;
+    const theme = options.getValue('platform') || options.getValue('theme');
+
+    // if the theme is 'markdown' load the base markdown theme
+    if (!theme || theme === 'markdown') {
+      options.setValue('theme', path.join(__dirname, 'theme'));
     }
-    return new MarkdownTheme(renderer, themePath, options);
+
+    // load any built in sub themes
+    const subThemes = ['docusaurus', 'vuepress', 'gitbook', 'bitbucket'];
+    if (subThemes.includes(theme)) {
+      options.setValue('theme', path.join(__dirname, 'theme', 'subthemes', theme));
+    }
   }
 }
