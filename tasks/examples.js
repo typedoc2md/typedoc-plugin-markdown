cb1f71ba7ac188aff9d1e21c5dc69926dead38f3
tgreyuk <tgreyuk@gmail.com>
2019-09-15 22:58:59 +0100

    Fixed output

--- tasks/examples.js
+++ tasks/examples.js
@@ -22,6 +22,6 @@ const argsWithPlugin = [...args, ...['--plugin', join(__dirname, '../dist/index.
 
 spawn('typedoc', [...args, ...['--out', 'out/default']], options);
 spawn('typedoc', [...argsWithPlugin, ...['--out', 'out/markdown']], options);
-spawn('typedoc', [...argsWithPlugin, ...['--theme', 'custom', '--out', 'out/docusaurus/docs/myapi']], options);
+spawn('typedoc', [...argsWithPlugin, ...['--theme', 'docusaurus', '--out', 'out/docusaurus/docs/myapi']], options);
 spawn('typedoc', [...argsWithPlugin, ...['--theme', 'bitbucket', '-out', 'out/bitbucket/docs']], options);
 spawn('typedoc', [...argsWithPlugin, ...['--theme', 'gitbook', '-out', 'out/gitbook/docs']], options);
