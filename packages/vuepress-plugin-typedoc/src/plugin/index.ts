5f1bbb29aabbda6439673e34176f73fe51e94972
tgreyuk <tgreyuk@gmail.com>
2020-08-24 11:35:41 +0100

    Navigation and frontmatter improvements

--- packages/vuepress-plugin-typedoc/src/plugin/index.ts
+++ packages/vuepress-plugin-typedoc/src/plugin/index.ts
@@ -1,86 +1,85 @@
 import * as path from 'path';
 
-import { Application } from 'typedoc';
-import { ModuleKind, ScriptTarget } from 'typescript';
+import * as fs from 'fs-extra';
+import { Application, NavigationItem } from 'typedoc';
 
 export const typedocPlugin = (options, ctx) => {
   let sidebarJson;
   let skipSidebar;
   let outFolder;
+  let generateSidebar = false;
   return {
     name: 'vuepress-plugin-typedoc',
 
     async ready() {
-      const sourceDir = ctx.sourceDir;
-      const inputFiles = options.inputFiles;
-      outFolder = options.out ? options.out : 'api';
-      const out = sourceDir + '/' + outFolder;
-      skipSidebar = options.skipSidebar || false;
+      try {
+        const sourceDir = ctx.sourceDir;
+        const inputFiles = options.inputFiles;
+        outFolder = options.out ? options.out : 'api';
+        const out = sourceDir + '/' + outFolder;
+        skipSidebar = options.skipSidebar || false;
+        options.hideBreadcrumbs = true;
 
-      delete options.skipSidebar;
-      delete options.inputFiles;
-      delete options.out;
+        delete options.skipSidebar;
+        delete options.inputFiles;
+        delete options.out;
 
-      const app = new Application();
-      app.bootstrap({
-        module: ModuleKind.CommonJS,
-        target: ScriptTarget.ES5,
-        disableOutputCheck: true,
-        readme: 'none',
-        plugin: ['typedoc-plugin-markdown'],
-        theme: path.resolve(__dirname, '..', 'theme'),
-        ...options,
-      });
+        const app = new Application();
+        app.bootstrap({
+          plugin: ['typedoc-plugin-markdown'],
+          theme: path.resolve(__dirname, '..', 'theme'),
+          ...options,
+        });
 
-      const project = app.convert(app.expandInputFiles(inputFiles));
-      app.generateDocs(project, out);
-
-      const navigation = app.renderer.theme.getNavigation(project);
-      sidebarJson = getSidebarJson(navigation);
+        const project = app.convert(app.expandInputFiles(inputFiles));
+        app.generateDocs(project, out);
+        const theme = app.renderer.theme as any;
+        const navigation = theme.getNavigationV3(project);
+        generateSidebar = app.renderer.theme!.isOutputDirectory(out) && !skipSidebar;
+        sidebarJson = getSidebarJson(navigation, outFolder);
+      } catch (e) {
+        //console.log(e);
+        return;
+      }
     },
 
     async enhanceAppFiles() {
       return {
         name: 'typedoc-sidebar',
-        content: `export default ({ siteData, options }) => { siteData.themeConfig.sidebar = Object.assign({},siteData.themeConfig.sidebar,${JSON.stringify(
-          skipSidebar
-            ? null
-            : {
-                [`/${outFolder}/`]: sidebarJson,
-              },
-        )}) }`,
+        content: `export default ({ siteData, options }) => {
+          siteData.themeConfig.sidebarDepth = 2;
+          siteData.themeConfig.sidebar = Object.assign({},siteData.themeConfig.sidebar,${JSON.stringify(
+            generateSidebar
+              ? {
+                  [`/${outFolder}/`]: sidebarJson,
+                }
+              : null,
+          )});
+        }`,
       };
     },
   };
 };
 
-function getSidebarJson(navigation) {
-  const navObject = {};
-  let url = '';
-  let navKey = '';
-  const titleMap = {};
-  navigation.children.forEach((rootNavigation) => {
-    rootNavigation.children.forEach((item) => {
-      url = item.url.replace('.md', '');
-      navKey = url.substr(0, url.indexOf('/'));
-      if (navKey !== undefined && navKey.length) {
-        navKey = navKey[0].toUpperCase() + navKey.slice(1);
-      }
-      if (navObject[navKey] === undefined) {
-        navObject[navKey] = [];
-      }
-      if (!navObject[navKey].includes(url)) {
-        titleMap[url] = item.title;
-        navObject[navKey].push(url);
-      }
-    });
+function getSidebarJson(navigation: NavigationItem, outFolder: string) {
+  const navJson = [];
+
+  navigation.children.forEach((navigationItem) => {
+    if (navigationItem.url && navigationItem.children.length === 0) {
+      const urlKey = navigationItem.url.replace('.md', '');
+      navJson.push([urlKey === 'README' ? `/${outFolder}/` : 'globals', navigationItem.title]);
+    } else {
+      navJson.push({
+        title: navigationItem.title,
+        children: navigationItem.children.map((navItem) => {
+          return [getUrlKey(navItem.url), navItem.title];
+        }),
+      });
+    }
   });
-  const navJson = Object.entries(navObject).map(([key, value]) => {
-    const items = value as any;
-    return {
-      title: key,
-      children: items.map((item) => [item, titleMap[item]]),
-    };
-  });
+  fs.outputFileSync('sidebars.json', JSON.stringify(navJson, null, 2));
   return navJson;
+}
+function getUrlKey(url: string) {
+  return url.replace('.md', '');
 }
--- packages/vuepress-plugin-typedoc/src/theme/theme.ts
+++ packages/vuepress-plugin-typedoc/src/theme/theme.ts
@@ -1,11 +1,11 @@
-import { FrontMatterComponent } from 'typedoc-plugin-markdown/dist/components/front-matter.component';
 import MarkdownTheme from 'typedoc-plugin-markdown/dist/theme';
 import { Renderer } from 'typedoc/dist/lib/output/renderer';
+
+import { VuepressFrontMatterComponent } from './front-matter-component';
 
 export default class DocusaurusTheme extends MarkdownTheme {
   constructor(renderer: Renderer, basePath: string) {
     super(renderer, basePath);
-    this.indexName = 'index';
-    renderer.addComponent('frontmatter', new FrontMatterComponent(renderer));
+    renderer.addComponent('vuepress-frontmatter', new VuepressFrontMatterComponent(renderer));
   }
 }
