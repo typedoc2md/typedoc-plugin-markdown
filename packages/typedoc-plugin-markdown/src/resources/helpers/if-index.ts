1bd0cfd1f80249471ab4fad51829481a2a19f86f
tgreyuk <tgreyuk@gmail.com>
2020-08-29 23:22:47 +0100

    Updated indexes

--- packages/typedoc-plugin-markdown/src/resources/helpers/if-index.ts
+++
@@ -1,5 +0,0 @@
-import { DeclarationReflection, ReflectionKind } from 'typedoc';
-
-export function ifIndex(this: DeclarationReflection, options) {
-  return !this.kind || this.kind === ReflectionKind.Module ? options.fn(this) : options.inverse(this);
-}
