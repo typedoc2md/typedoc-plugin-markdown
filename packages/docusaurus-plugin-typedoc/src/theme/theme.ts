f52373cb4cc1b50915f7e8399977ec02b8b8dc7d
tgreyuk <tgreyuk@gmail.com>
2020-09-03 00:39:18 +0100

    Replace all angle brackets for full `mdx` support (141)

--- packages/docusaurus-plugin-typedoc/src/theme/theme.ts
+++ packages/docusaurus-plugin-typedoc/src/theme/theme.ts
@@ -1,4 +1,5 @@
 import MarkdownTheme from 'typedoc-plugin-markdown/dist/theme';
+import { PageEvent } from 'typedoc/dist/lib/output/events';
 import { Renderer } from 'typedoc/dist/lib/output/renderer';
 
 import { DocsaurusFrontMatterComponent } from './front-matter-component';
@@ -6,10 +7,12 @@ import { DocsaurusFrontMatterComponent } from './front-matter-component';
 export default class DocusaurusTheme extends MarkdownTheme {
   constructor(renderer: Renderer, basePath: string) {
     super(renderer, basePath);
+    this.listenTo(renderer, PageEvent.END, this.onDocusaurusPageEnd, 1024);
     this.indexName = 'index';
-    renderer.addComponent(
-      'docusaurus-frontmatter',
-      new DocsaurusFrontMatterComponent(renderer),
-    );
+    renderer.addComponent('docusaurus-frontmatter', new DocsaurusFrontMatterComponent(renderer));
+  }
+
+  private onDocusaurusPageEnd(page: PageEvent) {
+    page.contents = page.contents.replace(/</g, '‹').replace(/>/g, '›');
   }
 }
