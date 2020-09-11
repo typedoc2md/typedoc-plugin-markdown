1fe85024c7d9d83ea1370137a4def1fe96ec7fad
tgreyuk <tgreyuk@gmail.com>
2020-09-11 10:11:06 +0100

    Reverted source file implementation

--- packages/typedoc-plugin-markdown/src/resources/helpers/source-file.ts
+++
@@ -1,4 +0,0 @@
-export function sourceFile(fileName: string, line: string, url: string) {
-  const md = url ? `[${fileName}:${line}](${url})` : `${fileName}:${line}`;
-  return 'Defined in Y' + md;
-}
