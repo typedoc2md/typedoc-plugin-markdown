6fd042d3387adc9104da5ee6c34f84083f6f145d
tgreyuk <tgreyuk@gmail.com>
2020-09-13 20:11:31 +0100

    Updated comment helper

--- packages/typedoc-plugin-markdown/src/resources/helpers/comment.ts
+++ packages/typedoc-plugin-markdown/src/resources/helpers/comment.ts
@@ -1,5 +1,25 @@
+import { Comment } from 'typedoc/dist/lib/models';
 import MarkdownTheme from '../../theme';
 
-export function comment(this: string) {
-  return MarkdownTheme.HANDLEBARS.helpers.comment.call(this);
+export function comment(this: Comment) {
+  const md: string[] = [];
+
+  if (this.shortText) {
+    md.push(MarkdownTheme.HANDLEBARS.helpers.comment.call(this.shortText));
+  }
+  if (this.text) {
+    md.push(MarkdownTheme.HANDLEBARS.helpers.comment.call(this.text));
+  }
+  if (this.tags) {
+    const tags = this.tags.map(
+      (tag) =>
+        `**\`${tag.tagName}\`** ${
+          tag.text
+            ? MarkdownTheme.HANDLEBARS.helpers.comment.call(tag.text)
+            : ''
+        }`,
+    );
+    md.push(tags.join('\n\n'));
+  }
+  return md.join('\n\n');
 }
