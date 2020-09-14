a63bef08b72143cc09959f16a60a14721b3f1e31
tgreyuk <tgreyuk@gmail.com>
2020-09-14 22:53:42 +0100

    Created theme file

---
+++ packages/docusaurus-plugin-typedoc/src/theme/theme.ts
@@ -0,0 +1,18 @@
+import MarkdownTheme from 'typedoc-plugin-markdown/dist/theme';
+import { PageEvent } from 'typedoc/dist/lib/output/events';
+import { Renderer } from 'typedoc/dist/lib/output/renderer';
+
+export default class DocusaurusTheme extends MarkdownTheme {
+  constructor(renderer: Renderer, basePath: string) {
+    super(renderer, basePath);
+    this.listenTo(renderer, PageEvent.END, this.onDocusaurusPageEnd, 1024);
+  }
+
+  private onDocusaurusPageEnd(page: PageEvent) {
+    if (page.contents) {
+      page.contents = page.contents
+        .replace(/\\</g, '&#60;')
+        .replace(/\\"/g, '&#34;');
+    }
+  }
+}
