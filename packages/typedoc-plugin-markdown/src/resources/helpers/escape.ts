416b452484ec88cccaf373449bb5c3694d24c5e5
tgreyuk <tgreyuk@gmail.com>
2020-09-14 20:49:00 +0100

    Added escape characters

--- packages/typedoc-plugin-markdown/src/resources/helpers/escape.ts
+++ packages/typedoc-plugin-markdown/src/resources/helpers/escape.ts
@@ -1,3 +1,10 @@
 export function escape(str: string) {
-  return str.replace(/</g, '\\<').replace(/_/g, '\\_').replace(/`/g, '\\`');
+  return str
+    .replace(/</g, '\\<')
+    .replace(/_/g, '\\_')
+    .replace(/`/g, '\\`')
+    .replace(/\|/g, '\\|')
+    .replace(/\n/g, '\\n')
+    .replace(/\r/g, '\\r')
+    .replace(/\t/g, '\\t');
 }
