efbc94bde4f08703118fe64754d5efe20805df8d
tgreyuk <tgreyuk@gmail.com>
2020-10-29 21:14:36 +0000

    Remove legacy theme messages

--- packages/typedoc-plugin-markdown/src/plugin.ts
+++ packages/typedoc-plugin-markdown/src/plugin.ts
@@ -33,15 +33,7 @@ export class MarkdownPlugin extends ConverterComponent {
    * Load markdown theme and perform additional checks
    */
   onResolveBegin() {
-    // v2 legacy markdown themes
-    const legacyThemes = [
-      'bitbucket',
-      'docusaurus',
-      'docusaurus2',
-      'vuepress',
-      'gitbook',
-    ];
-    if (![...legacyThemes, ...['default', 'markdown']].includes(this.theme)) {
+    if (!['default', 'markdown'].includes(this.theme)) {
       // For custom themes check that the theme is a markdown theme
       // If it is return and pass through to renderer
       const themeFileName = path.resolve(path.join(this.theme, 'theme.js'));
@@ -51,14 +43,6 @@ export class MarkdownPlugin extends ConverterComponent {
       this.application.logger.warn(
         `[typedoc-plugin-markdown] '${this.theme}' is not a recognised markdown theme. If an html theme is required, please disable this plugin.`,
       );
-    }
-
-    // Graceful upgrade for legacy themes
-    if (legacyThemes.includes(this.theme)) {
-      this.application.logger.warn(
-        `[typedoc-plugin-markdown] Please note the ${this.theme} theme is no longer supported as of v3.0.0.`,
-      );
-      this.upgradeMessages(this.theme);
     }
 
     // Set the default markdown theme
@@ -74,24 +58,6 @@ export class MarkdownPlugin extends ConverterComponent {
       return ThemeClass.prototype instanceof MarkdownTheme;
     } catch (e) {
       return false;
-    }
-  }
-
-  upgradeMessages(theme: string) {
-    if (theme.startsWith('docusaurus')) {
-      this.application.logger.warn(
-        `[typedoc-plugin-markdown] Please use https://www.npmjs.com/package/docusaurus-plugin-typedoc`,
-      );
-    }
-    if (theme === 'vuepress') {
-      this.application.logger.warn(
-        `[typedoc-plugin-markdown] Please use https://www.npmjs.com/package/vuepress-plugin-typedoc`,
-      );
-    }
-    if (theme === 'bitbucket') {
-      this.application.logger.warn(
-        '[typedoc-plugin-markdown] Please use https://www.npmjs.com/package/typedoc-bitbucket-theme',
-      );
     }
   }
 }
