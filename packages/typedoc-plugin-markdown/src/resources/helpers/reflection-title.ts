c4589868439dcf9673c55224d76a47dc6b1daf86
tgreyuk <tgreyuk@gmail.com>
2021-01-05 19:33:50 +0000

    Allow custom index title

--- packages/typedoc-plugin-markdown/src/resources/helpers/reflection-title.ts
+++ packages/typedoc-plugin-markdown/src/resources/helpers/reflection-title.ts
@@ -1,20 +1,27 @@
 import { ParameterReflection } from 'typedoc';
 import { PageEvent } from 'typedoc/dist/lib/output/events';
 
+import MarkdownTheme from '../../theme';
 import { escape } from './escape';
 
 export function reflectionTitle(this: PageEvent, shouldEscape = true) {
-  const title: string[] = [];
+  const title: string[] = [''];
 
   if (this.model.kindString && this.url !== this.project.url) {
     title.push(`${this.model.kindString}: `);
   }
-  title.push(shouldEscape ? escape(this.model.name) : this.model.name);
-  if (this.model.typeParameters) {
-    const typeParameters = this.model.typeParameters
-      .map((typeParameter: ParameterReflection) => typeParameter.name)
-      .join(', ');
-    title.push(`<${typeParameters}${shouldEscape ? '\\>' : '>'}`);
+  if (this.url === this.project.url) {
+    title.push(
+      MarkdownTheme.HANDLEBARS.helpers.indexTitle() || this.project.name,
+    );
+  } else {
+    title.push(shouldEscape ? escape(this.model.name) : this.model.name);
+    if (this.model.typeParameters) {
+      const typeParameters = this.model.typeParameters
+        .map((typeParameter: ParameterReflection) => typeParameter.name)
+        .join(', ');
+      title.push(`<${typeParameters}${shouldEscape ? '\\>' : '>'}`);
+    }
   }
   return title.join('');
 }
