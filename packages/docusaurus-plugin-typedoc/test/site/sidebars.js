016818367f3e396bd90ad012b807bf21ef8d97c8
tgreyuk <tgreyuk@gmail.com>
2021-01-24 21:50:44 +0000

    Added docusaurus test site

---
+++ packages/docusaurus-plugin-typedoc/test/site/sidebars.js
@@ -0,0 +1,8 @@
+module.exports = {
+  someSidebar: {
+    Docusaurus: ['doc1', 'doc2', 'doc3'],
+    Features: ['mdx'],
+    API1: require('./sidebars/typedoc-sidebar-1.js'),
+    API2: require('./sidebars/typedoc-sidebar-2.js'),
+  },
+};
