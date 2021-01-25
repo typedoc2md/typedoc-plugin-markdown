ca9e09dad5ff5c234c619b431186eb957658071d
tgreyuk <tgreyuk@gmail.com>
2021-01-25 23:08:09 +0000

    Fixed spec

--- packages/docusaurus-plugin-typedoc/test/specs/sidebar.spec.ts
+++ packages/docusaurus-plugin-typedoc/test/specs/sidebar.spec.ts
@@ -19,7 +19,7 @@ async function generate(opts = {}) {
 
   const options = getOptions(tmpobj.name, opts);
 
-  await app.bootstrap({
+  app.bootstrap({
     ...options,
     logger: 'none',
     plugin: [
