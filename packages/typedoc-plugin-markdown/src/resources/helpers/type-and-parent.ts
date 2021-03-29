cb74e83116046aebb900f25f8348a74094b03901
Masashi Kojo <masashi.kojo@toshiba.co.jp>
2021-03-29 11:54:48 +0900

    fix: Override section of methods overriding those of classes from node modules

--- packages/typedoc-plugin-markdown/src/resources/helpers/type-and-parent.ts
+++ packages/typedoc-plugin-markdown/src/resources/helpers/type-and-parent.ts
@@ -4,50 +4,58 @@ import { ArrayType, ReferenceType } from 'typedoc/dist/lib/models/types';
 import MarkdownTheme from '../../theme';
 
 export function typeAndParent(this: ArrayType | ReferenceType) {
-  if (this instanceof ReferenceType && this.reflection) {
-    const md: string[] = [];
-    const parentReflection = this.reflection.parent;
-    if (this.reflection instanceof SignatureReflection) {
-      if (
-        parentReflection &&
-        parentReflection.parent &&
-        parentReflection.parent.url
-      ) {
-        md.push(
-          `[${
-            parentReflection.parent.name
-          }](${MarkdownTheme.HANDLEBARS.helpers.relativeURL(
-            parentReflection.parent.url,
-          )})`,
-        );
-      } else if (parentReflection && parentReflection.parent) {
-        md.push(parentReflection.parent.name);
-      }
+  if (this) {
+    if ("elementType" in this) {
+      return typeAndParent.call(this.elementType) + '[]';
     } else {
-      if (parentReflection && parentReflection.url) {
-        md.push(
-          `[${
-            parentReflection.name
-          }](${MarkdownTheme.HANDLEBARS.helpers.relativeURL(
-            parentReflection.url,
-          )})`,
-        );
-      } else if (parentReflection) {
-        md.push(parentReflection.name);
-      }
-      if (this.reflection.url) {
-        md.push(
-          `[${
-            this.reflection.name
-          }](${MarkdownTheme.HANDLEBARS.helpers.relativeURL(
-            this.reflection.url,
-          )})`,
-        );
+      if (this.reflection) {
+        const md: string[] = [];
+        const parentReflection = this.reflection.parent;
+        if (this.reflection instanceof SignatureReflection) {
+          if (
+            parentReflection &&
+            parentReflection.parent &&
+            parentReflection.parent.url
+          ) {
+            md.push(
+              `[${
+                parentReflection.parent.name
+              }](${MarkdownTheme.HANDLEBARS.helpers.relativeURL(
+                parentReflection.parent.url,
+              )})`,
+            );
+          } else if (parentReflection && parentReflection.parent) {
+            md.push(parentReflection.parent.name);
+          }
+        } else {
+          if (parentReflection && parentReflection.url) {
+            md.push(
+              `[${
+                parentReflection.name
+              }](${MarkdownTheme.HANDLEBARS.helpers.relativeURL(
+                parentReflection.url,
+              )})`,
+            );
+          } else if (parentReflection) {
+            md.push(parentReflection.name);
+          }
+          if (this.reflection.url) {
+            md.push(
+              `[${
+                this.reflection.name
+              }](${MarkdownTheme.HANDLEBARS.helpers.relativeURL(
+                this.reflection.url,
+              )})`,
+            );
+          } else {
+            md.push(this.reflection.name);
+          }
+        }
+        return md.join('.');
       } else {
-        md.push(this.reflection.name);
+        return this.toString();
       }
     }
-    return md.join('.');
   }
   return 'void';
 }
