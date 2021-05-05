2e898ac1ec7b00a610da7d57d90a155f649d64a7
tgreyuk <tgreyuk@gmail.com>
2021-05-05 21:16:04 +0100

    feat: Improve heading structure with disabled inline toc (#222)

--- packages/typedoc-plugin-markdown/src/components/toc.ts
+++ packages/typedoc-plugin-markdown/src/components/toc.ts
@@ -1,12 +1,14 @@
 import { BindOption } from 'typedoc';
 import {
   DeclarationReflection,
+  ProjectReflection,
   ReflectionGroup,
 } from 'typedoc/dist/lib/models';
 import {
   Component,
   ContextAwareRendererComponent,
 } from 'typedoc/dist/lib/output/components';
+
 import { escape } from '../resources/helpers/escape';
 import MarkdownTheme from '../theme';
 
@@ -20,7 +22,7 @@ export class TableOfContents extends ContextAwareRendererComponent {
 
     MarkdownTheme.HANDLEBARS.registerHelper(
       'toc',
-      (reflection: DeclarationReflection) => {
+      (reflection: ProjectReflection | DeclarationReflection) => {
         const md: string[] = [];
         const isVisible = reflection.groups?.some((group) =>
           group.allChildrenHaveOwnDocument(),
@@ -29,18 +31,21 @@ export class TableOfContents extends ContextAwareRendererComponent {
           (!this.hideInPageTOC && reflection.groups) ||
           (isVisible && reflection.groups)
         ) {
-          md.push(`## Table of contents\n\n`);
+          if (!this.hideInPageTOC) {
+            md.push(`## Table of contents\n\n`);
+          }
+          const headingLevel = this.hideInPageTOC ? `##` : `###`;
           reflection.groups?.forEach((group) => {
             const groupTitle = group.title;
             if (group.categories) {
               group.categories.forEach((category) => {
-                md.push(`### ${category.title} ${groupTitle}\n\n`);
+                md.push(`${headingLevel} ${category.title} ${groupTitle}\n\n`);
                 pushGroup(category as any, md);
                 md.push('\n');
               });
             } else {
               if (!this.hideInPageTOC || group.allChildrenHaveOwnDocument()) {
-                md.push(`### ${groupTitle}\n\n`);
+                md.push(`${headingLevel} ${groupTitle}\n\n`);
                 pushGroup(group, md);
                 md.push('\n');
               }
