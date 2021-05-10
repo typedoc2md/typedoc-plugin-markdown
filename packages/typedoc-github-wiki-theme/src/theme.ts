5d0654f409a0c315d255117550e304894a21fe51
tgreyuk <tgreyuk@gmail.com>
2021-05-10 21:49:52 +0100

    feat: Improved sidebar structure

--- packages/typedoc-github-wiki-theme/src/theme.ts
+++ packages/typedoc-github-wiki-theme/src/theme.ts
@@ -1,6 +1,13 @@
 import * as fs from 'fs-extra';
-import { DeclarationReflection, NavigationItem, Reflection } from 'typedoc';
+import {
+  BindOption,
+  DeclarationReflection,
+  NavigationItem,
+  ProjectReflection,
+  Reflection,
+} from 'typedoc';
 import MarkdownTheme from 'typedoc-plugin-markdown/dist/theme';
+import { GroupPlugin } from 'typedoc/dist/lib/converter/plugins';
 import { RendererEvent } from 'typedoc/dist/lib/output/events';
 import { Renderer } from 'typedoc/dist/lib/output/renderer';
 import { TemplateMapping } from 'typedoc/dist/lib/output/themes/DefaultTheme';
@@ -9,31 +16,37 @@ import { UtilsComponent } from './components/utils';
 
 export default class GithubWikiTheme extends MarkdownTheme {
   renderer: Renderer;
+  @BindOption('entryPoints')
+  entryPoints!: string[];
+  @BindOption('readme')
+  readme!: string;
+
   constructor(renderer: Renderer, basePath: string) {
     super(renderer, basePath);
     this.renderer = renderer;
-    renderer.addComponent('github-wiki-utils', new UtilsComponent(renderer));
-
     this.renderer.application.options.setValue('entryDocument', 'Home.md');
-    this.renderer.application.options.setValue('hideInPageTOC', true);
     this.renderer.application.options.setValue('hideBreadcrumbs', true);
-
+    this.renderer.application.options.setValue('hidePageTitle', true);
+    renderer.addComponent('utils', new UtilsComponent(renderer));
     this.listenTo(renderer, RendererEvent.END, this.onRendererEnd, 1024);
   }
+
+  getUrls(project: ProjectReflection) {
+    return super.getUrls(project).map((urlMapping) => {
+      if (this.readme !== 'none' && project.url === urlMapping.url) {
+        return {
+          ...urlMapping,
+          url: this.entryPoints.length > 1 ? 'Modules.md' : 'Exports.md',
+        };
+      }
+      return urlMapping;
+    });
+  }
+
   toUrl(mapping: TemplateMapping, reflection: DeclarationReflection) {
-    const kindLabelMap = {
-      modules: 'Module',
-      classes: 'Class',
-      interfaces: 'Interface',
-      functions: 'Function',
-      enums: 'Enum',
-    };
-    return (
-      (kindLabelMap[mapping.directory] || mapping.directory) +
-      ': ' +
-      this.getUrl(reflection) +
-      '.md'
-    );
+    return `${GroupPlugin.getKindSingular(reflection.kind)}: ${this.getUrl(
+      reflection,
+    )}.md`;
   }
 
   getUrl(reflection: Reflection): string {
@@ -44,33 +57,31 @@ export default class GithubWikiTheme extends MarkdownTheme {
   onRendererEnd(renderer: RendererEvent) {
     const parseUrl = (url: string) => '../wiki/' + url.replace('.md', '');
     const navigation: NavigationItem = this.getNavigation(renderer.project);
-    const navJson: string[] = [
-      `## [${renderer.project.name}](${parseUrl(this.entryDocument)})\n`,
-    ];
-    if (this.readme !== 'none') {
-      navJson.push(`- [Modules](${parseUrl(this.globalsFile)})`);
-    }
-    if (navigation.children) {
-      navigation.children.forEach((navItem) => {
-        if (navItem.isLabel) {
-          navJson.push(`\n### ${navItem.title}\n`);
-          navItem.children?.forEach((navItemChild) => {
-            const longTitle = navItemChild.title.split('.');
-            const shortTitle = longTitle[longTitle.length - 1];
-            navJson.push(
-              `- [${shortTitle}](${parseUrl(encodeURI(navItemChild.url))})`,
-            );
-          });
-        }
-      });
-    }
+    const navJson: string[] = [`## ${renderer.project.name}\n`];
+
+    navigation.children?.forEach((navItem) => {
+      if (navItem.isLabel) {
+        navJson.push(`\n### ${navItem.title}\n`);
+        navItem.children?.forEach((navItemChild) => {
+          const longTitle = navItemChild.title.split('.');
+          const shortTitle = longTitle[longTitle.length - 1];
+          navJson.push(
+            `- [${shortTitle}](${parseUrl(encodeURI(navItemChild.url))})`,
+          );
+        });
+      } else {
+        const title =
+          navItem.url === this.entryDocument ? 'Home' : navItem.title;
+        navJson.push(`- [${title}](${parseUrl(navItem.url)})`);
+      }
+    });
     fs.outputFileSync(
       renderer.outputDirectory + '/_Sidebar.md',
-      navJson.join('\n'),
+      navJson.join('\n') + '\n',
     );
   }
 
   get globalsFile() {
-    return 'Modules.md';
+    return this.entryPoints.length > 1 ? 'Modules.md' : 'Exports.md';
   }
 }
