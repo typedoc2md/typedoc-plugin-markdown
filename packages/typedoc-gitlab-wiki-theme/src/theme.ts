dea107b215095881ef858513362478b14637fdd0
tgreyuk <tgreyuk@gmail.com>
2021-05-10 21:56:45 +0100

    feat: Improved sidebar structure

--- packages/typedoc-gitlab-wiki-theme/src/theme.ts
+++ packages/typedoc-gitlab-wiki-theme/src/theme.ts
@@ -28,24 +28,30 @@ export default class GitlabTheme extends MarkdownTheme {
   onGitLabRendererEnd(renderer: RendererEvent) {
     const parseUrl = (url: string) => url.replace(/(.*).md/, '$1');
     const navigation: NavigationItem = this.getNavigation(renderer.project);
-    const navJson: string[] = [
-      `## [${renderer.project.name}](${parseUrl(this.entryDocument)})\n`,
-    ];
-    if (navigation.children) {
-      navigation.children.forEach((navItem) => {
-        if (navItem.isLabel) {
-          navJson.push(`\n### ${navItem.title}\n`);
-          navItem.children?.forEach((navItemChild) => {
-            const longTitle = navItemChild.title.split('.');
-            const shortTitle = longTitle[longTitle.length - 1];
-            navJson.push(`- [${shortTitle}](${parseUrl(navItemChild.url)})`);
-          });
-        }
-      });
-    }
+    const navJson: string[] = [`## ${renderer.project.name}\n`];
+    navigation.children?.forEach((navItem) => {
+      if (navItem.isLabel) {
+        navJson.push(`\n### ${navItem.title}\n`);
+        navItem.children?.forEach((navItemChild) => {
+          const longTitle = navItemChild.title.split('.');
+          const shortTitle = longTitle[longTitle.length - 1];
+          navJson.push(
+            `- [${shortTitle}](${parseUrl(encodeURI(navItemChild.url))})`,
+          );
+        });
+      } else {
+        const title =
+          navItem.url === this.entryDocument ? 'Home' : navItem.title;
+        navJson.push(`- [${title}](${parseUrl(navItem.url)})`);
+      }
+    });
     fs.writeFileSync(
       renderer.outputDirectory + '/_sidebar.md',
       navJson.join('\n'),
     );
+  }
+
+  get globalsFile() {
+    return this.entryPoints.length > 1 ? 'Modules.md' : 'Exports.md';
   }
 }
