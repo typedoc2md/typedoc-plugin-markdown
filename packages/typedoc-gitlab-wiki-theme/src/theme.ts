8eb7b3c5f850612b7af1a5dfefb4b9f741d7953b
tgreyuk <tgreyuk@gmail.com>
2022-12-11 20:18:36 +0000

    node

--- packages/typedoc-gitlab-wiki-theme/src/theme.ts
+++ packages/typedoc-gitlab-wiki-theme/src/theme.ts
@@ -1,6 +1,4 @@
-import * as fs from 'fs';
-
-import { Renderer, DeclarationReflection, RendererEvent } from 'typedoc';
+import { Renderer } from 'typedoc';
 import { MarkdownTheme } from 'typedoc-plugin-markdown';
 
 export class GitlabWikiTheme extends MarkdownTheme {
@@ -8,50 +6,8 @@ export class GitlabWikiTheme extends MarkdownTheme {
     super(renderer);
 
     this.entryDocument = 'home.md';
-    this.hideBreadcrumbs = true;
-    this.hidePageTitle = true;
-
-    this.listenTo(this.owner, {
-      [RendererEvent.END]: this.onGitLabRendererEnd,
-    });
-  }
-
-  getRelativeUrl(url: string) {
-    const relativeUrl = super
-      .getRelativeUrl(url)
-      .replace(/(.*).md/, '$1')
-      .replace(/ /g, '-');
-    return relativeUrl.startsWith('..') ? relativeUrl : './' + relativeUrl;
-  }
-
-  toUrl(mapping: any, reflection: DeclarationReflection) {
-    return `${mapping.directory}/${reflection.getFullName()}.md`;
-  }
-
-  onGitLabRendererEnd(renderer: RendererEvent) {
-    const parseUrl = (url: string) => url.replace(/(.*).md/, '$1');
-    const navigation = this.getNavigation(renderer.project);
-    const navJson: string[] = [`## ${renderer.project.name}\n`];
-    navigation.children?.forEach((navItem) => {
-      if (navItem.isLabel) {
-        navJson.push(`\n### ${navItem.title}\n`);
-        navItem.children?.forEach((navItemChild) => {
-          const longTitle = navItemChild.title.split('.');
-          const shortTitle = longTitle[longTitle.length - 1];
-          navJson.push(
-            `- [${shortTitle}](${parseUrl(encodeURI(navItemChild.url))})`,
-          );
-        });
-      } else {
-        const title =
-          navItem.url === this.entryDocument ? 'Home' : navItem.title;
-        navJson.push(`- [${title}](${parseUrl(navItem.url)})`);
-      }
-    });
-    fs.writeFileSync(
-      renderer.outputDirectory + '/_sidebar.md',
-      navJson.join('\n'),
-    );
+    // this.hideBreadcrumbs = true;
+    // this.hidePageTitle = true;
   }
 
   get globalsFile() {
