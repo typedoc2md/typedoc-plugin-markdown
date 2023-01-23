7412e431638ac26876bf38d48b2971300dfac93a
tgreyuk <tgreyuk@gmail.com>
2023-01-23 17:17:57 +0000

    changeset

---
+++ .changeset/breezy-avocados-lie.md
@@ -0,0 +1,5 @@
+---
+'typedoc-plugin-markdown': patch
+---
+
+- Respect monorepo `readmeFile` configuration (#383)
--- scripts/changesets/changelog.js
+++ scripts/changesets/changelog.js
@@ -1,14 +1,8 @@
 const getReleaseLine = async (changeset, _type) => {
-  /*const [firstLine, ...futureLines] = changeset.summary
-    .split('\n')
-    .map((l) => l.trimRight());
-
-   let returnVal = `- ${firstLine}`;
-
-  if (futureLines.length > 0) {
-    returnVal += `\n- ${futureLines.map((l) => `  ${l}`).join('\n')}`;
-  }*/
-  return changeset.summary;
+  return changeset.summary.replace(
+    /\(\#(.+?)\)/g,
+    '([#$1](https://github.com/tgreyuk/typedoc-plugin-markdown/issues/$1))',
+  );
 };
 
 const getDependencyReleaseLine = async (changesets, dependenciesUpdated) => {
