7bcad3d49d9bb5a142bc1533b45a86bb079f1722
tgreyuk <tgreyuk@gmail.com>
2023-01-23 22:02:14 +0000

    fix: p

--- scripts/changesets/changelog.js
+++ scripts/changesets/changelog.js
@@ -1,124 +1,34 @@
-const {
-  getInfo,
-  getInfoFromPullRequest,
-} = require('@changesets/get-github-info');
+const getReleaseLine = async (changeset, _type) => {
+  /*const [firstLine, ...futureLines] = changeset.summary
+    .split('\n')
+    .map((l) => l.trimRight());
 
-const changelogFunctions = {
-  getDependencyReleaseLine: async (
-    changesets,
-    dependenciesUpdated,
-    options,
-  ) => {
-    if (!options.repo) {
-      throw new Error(
-        'Please provide a repo to this changelog generator like this:\n"changelog": ["@changesets/changelog-github", { "repo": "org/repo" }]',
-      );
-    }
-    if (dependenciesUpdated.length === 0) return '';
+   let returnVal = `- ${firstLine}`;
 
-    const changesetLink = `- Updated dependencies [${(
-      await Promise.all(
-        changesets.map(async (cs) => {
-          if (cs.commit) {
-            let { links } = await getInfo({
-              repo: options.repo,
-              commit: cs.commit,
-            });
-            return links.commit;
-          }
-        }),
-      )
-    )
-      .filter((_) => _)
-      .join(', ')}]:`;
-
-    const updatedDepenenciesList = dependenciesUpdated.map(
-      (dependency) => `  - ${dependency.name}@${dependency.newVersion}`,
-    );
-
-    return [changesetLink, ...updatedDepenenciesList].join('\n');
-  },
-  getReleaseLine: async (changeset, type, options) => {
-    if (!options || !options.repo) {
-      throw new Error(
-        'Please provide a repo to this changelog generator like this:\n"changelog": ["@changesets/changelog-github", { "repo": "org/repo" }]',
-      );
-    }
-
-    let prFromSummary;
-    let commitFromSummary;
-    let usersFromSummary = [];
-
-    const replacedChangelog = changeset.summary
-      .replace(/^\s*(?:pr|pull|pull\s+request):\s*#?(\d+)/im, (_, pr) => {
-        let num = Number(pr);
-        if (!isNaN(num)) prFromSummary = num;
-        return '';
-      })
-      .replace(/^\s*commit:\s*([^\s]+)/im, (_, commit) => {
-        commitFromSummary = commit;
-        return '';
-      })
-      .replace(/^\s*(?:author|user):\s*@?([^\s]+)/gim, (_, user) => {
-        usersFromSummary.push(user);
-        return '';
-      })
-      .trim();
-
-    const [firstLine, ...futureLines] = replacedChangelog
-      .split('\n')
-      .map((l) => l.trimRight());
-
-    const links = await (async () => {
-      if (prFromSummary !== undefined) {
-        let { links } = await getInfoFromPullRequest({
-          repo: options.repo,
-          pull: prFromSummary,
-        });
-        if (commitFromSummary) {
-          links = {
-            ...links,
-            commit: `[\`${commitFromSummary}\`](https://github.com/${options.repo}/commit/${commitFromSummary})`,
-          };
-        }
-        return links;
-      }
-      const commitToFetchFrom = commitFromSummary || changeset.commit;
-      if (commitToFetchFrom) {
-        let { links } = await getInfo({
-          repo: options.repo,
-          commit: commitToFetchFrom,
-        });
-        return links;
-      }
-      return {
-        commit: null,
-        pull: null,
-        user: null,
-      };
-    })();
-
-    const users = usersFromSummary.length
-      ? usersFromSummary
-          .map(
-            (userFromSummary) =>
-              `[@${userFromSummary}](https://github.com/${userFromSummary})`,
-          )
-          .join(', ')
-      : links.user;
-
-    const prefix = [
-      links.pull === null ? '' : ` ${links.pull}`,
-      links.commit === null ? '' : ` ${links.commit}`,
-      users === null ? '' : ` Thanks ${users}!`,
-    ].join('');
-
-    return `\n\n-${prefix ? `${prefix} -` : ''} ${firstLine}\n${futureLines
-      .map((l) => `  ${l}`)
-      .join('\n')}`;
-  },
+  if (futureLines.length > 0) {
+    returnVal += `\n- ${futureLines.map((l) => `  ${l}`).join('\n')}`;
+  }*/
+  return changeset.summary.replace(
+    /\(\#(.+?)\)/g,
+    '[#$1](https://github.com/tgreyuk/typedoc-plugin-markdown/issues/$1)',
+  );
 };
 
-module.exports = {
-  default: changelogFunctions,
+const getDependencyReleaseLine = async (changesets, dependenciesUpdated) => {
+  if (dependenciesUpdated.length === 0) return '';
+
+  const updatedDependenciesList = dependenciesUpdated.map(
+    (dependency) => `  - ${dependency.name}@${dependency.newVersion}`,
+  );
+
+  return [['- Updated peer dependencies'], ...updatedDependenciesList].join(
+    '\n',
+  );
 };
+
+const defaultChangelogFunctions = {
+  getReleaseLine,
+  getDependencyReleaseLine,
+};
+
+exports.default = defaultChangelogFunctions;
