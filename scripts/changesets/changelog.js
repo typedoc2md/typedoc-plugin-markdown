12082678a31c2d351898791706f4c6c7ac0df8f4
tgreyuk <tgreyuk@gmail.com>
2023-04-11 12:34:32 +0100

    fix: Added users to changeset script

--- scripts/changesets/changelog.js
+++ scripts/changesets/changelog.js
@@ -1,10 +1,3 @@
-const getReleaseLine = async (changeset, _type) => {
-  return changeset.summary.replace(
-    /\(\#(.+?)\)/g,
-    '([#$1](https://github.com/tgreyuk/typedoc-plugin-markdown/issues/$1))',
-  );
-};
-
 const getDependencyReleaseLine = async (changesets, dependenciesUpdated) => {
   if (dependenciesUpdated.length === 0) return '';
 
@@ -15,6 +8,30 @@ const getDependencyReleaseLine = async (changesets, dependenciesUpdated) => {
   return [['- Updated peer dependencies'], ...updatedDependenciesList].join(
     '\n',
   );
+};
+
+const getReleaseLine = async (changeset, _type) => {
+  let usersFromSummary = [];
+  changeset.summary
+    .replace(/^\s*(?:author|user):\s*@?([^\s]+)/gim, (_, user) => {
+      usersFromSummary.push(user);
+      return '';
+    })
+    .trim();
+  const users = usersFromSummary.length
+    ? usersFromSummary
+        .map(
+          (userFromSummary) =>
+            `[@${userFromSummary}](https://github.com/${userFromSummary})`,
+        )
+        .join(', ')
+    : null;
+  const summary = changeset.summary.replace(
+    /\(\#(.+?)\)/g,
+    '([#$1](https://github.com/tgreyuk/typedoc-plugin-markdown/issues/$1))',
+  );
+
+  return [summary, users].join('\n\n');
 };
 
 const defaultChangelogFunctions = {
