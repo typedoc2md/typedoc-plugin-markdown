bd353da27266f91631bd68e06e5547b7baf78586
tgreyuk <tgreyuk@gmail.com>
2023-04-21 12:59:09 +0100

    chore: Updated tests

--- packages/docusaurus-plugin-typedoc/test/specs/options.spec.ts
+++ packages/docusaurus-plugin-typedoc/test/specs/options.spec.ts
@@ -2,19 +2,6 @@ import { getPluginOptions } from '../../dist/options';
 import { PluginOptions } from '../../src/models';
 
 describe(`Options:`, () => {
-  test(`should return default options`, () => {
-    expect(
-      getPluginOptions({ entryPoints: ['entrypoint.ts'] }),
-    ).toMatchSnapshot();
-  });
-  test(`should merge plugins`, () => {
-    expect(
-      getPluginOptions({
-        plugin: ['plugin-xyz'],
-      }).plugin,
-    ).toMatchSnapshot();
-  });
-
   test(`should merge custom sidebar options`, () => {
     expect(
       getPluginOptions({
