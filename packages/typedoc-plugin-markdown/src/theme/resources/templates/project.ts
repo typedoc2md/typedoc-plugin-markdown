844e1a90af37afe5aa9e8ceafced789de92fd6e1
tgreyuk <tgreyuk@gmail.com>
2024-01-10 00:10:10 +0000

    fix: Remove page title from merged readme.

--- packages/typedoc-plugin-markdown/src/theme/resources/templates/project.ts
+++ packages/typedoc-plugin-markdown/src/theme/resources/templates/project.ts
@@ -21,25 +21,23 @@ export function projectTemplate(
   }
 
   const includeReadme =
-    context.options.getValue('mergeReadme') && page.model.readme;
-
-  const headingLevel = includeReadme ? 2 : 1;
+    context.options.getValue('mergeReadme') && Boolean(page.model.readme);
 
   if (includeReadme && page.model.readme) {
     md.push(context.commentParts(page.model.readme));
   }
 
-  if (!context.options.getValue('hidePageTitle')) {
-    md.push(heading(headingLevel, context.pageTitle(page)));
+  if (!context.options.getValue('hidePageTitle') && !includeReadme) {
+    md.push(heading(1, context.pageTitle(page)));
   }
 
   if (page.model.comment) {
-    md.push(context.comment(page.model.comment, headingLevel + 1));
+    md.push(context.comment(page.model.comment, 2));
   }
 
-  md.push(context.pageIndex(page, headingLevel + 1));
+  md.push(context.pageIndex(page, 2));
 
-  md.push(context.members(page.model, headingLevel + 1));
+  md.push(context.members(page.model, 2));
 
   md.push(context.footer());
 
