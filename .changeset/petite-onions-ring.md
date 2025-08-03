f98c0b6e1d630ed0777b2ba2ecaa024c7236f8ae
tgreyuk <tgreyuk@gmail.com>
2025-08-04 00:00:42 +0100

    fix(core): correctly handle empty markdown links

---
+++ .changeset/petite-onions-ring.md
@@ -0,0 +1,5 @@
+---
+'typedoc-plugin-markdown': patch
+---
+
+- Correctly handle empty markdown links (#836).
--- packages/typedoc-plugin-markdown/src/libs/markdown/link.ts
+++ packages/typedoc-plugin-markdown/src/libs/markdown/link.ts
@@ -1,8 +1,11 @@
 /**
- *  The link element
+ * The link element
  * @param label The text to display for the link
- * @param url The url to link to
+ * @param url The URL to link to
+ * @returns A Markdown link if URL is provided, otherwise plain text
  */
-export function link(label: string, url: string | null) {
-  return `[${label.trim()}](${url || ''})`;
+export function link(label: string, url?: string): string {
+  const parsedUrl = url?.trim() || '';
+  const safeLabel = label.trim();
+  return parsedUrl.length ? `[${safeLabel}](${parsedUrl})` : safeLabel;
 }
